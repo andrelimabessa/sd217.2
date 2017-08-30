8a43c41b1a8597c817c0fff9ca3ca6a8b41f80fd
André Bessa <andrebessa@gmail.com>
2017-08-30 14:30:13 -0300

    revertendo commit

--- 1.CampoMinadoMonolitico/campo_minado_negocio.py
+++ 1.CampoMinadoMonolitico/campo_minado_negocio.py
@@ -2,28 +2,40 @@ from random import randint
 
 class CampoMinado:
 
-    def __init__(self, tamanho):
-        """ Iniciando objeto com tabuleiro NxN """
-        self.tamanho = tamanho
-        self.coordenadas_bombas = []
-        self.qtd_bombas = self.__total_bombas()
-        self.__distribuir_bombas()
-        self.tabuleiro = [['X' for i in range(tamanho)] for j in range(tamanho)]
+    def __init__(self, linha, coluna):
+        """ Inicializando campo minado com linha X coluna posicoes """
+        self.__linha = linha
+        self.__coluna = coluna
+        self.__tabuleiro = self.__inicializar_tabuleiro(linha, coluna)
+        self.__coordenadas_bombas = self.__distribuir_bombas(linha,coluna)
+        
+    def __inicializar_tabuleiro(self, linha, coluna):
+        return [['X' for x in range(coluna)] for j in range(linha)]
 
-    def __distribuir_bombas(self):
-        num_bombas = self.qtd_bombas
-        while num_bombas > 0:
-            coordenada = (randint(0, self.tamanho),randint(0, self.tamanho))
-            if coordenada not in self.coordenadas_bombas:
-                self.coordenadas_bombas.append(coordenada)
-                num_bombas-=1
+    def __distribuir_bombas(self, linha, coluna):
+        quantidade_bombas = self.__total_bombas(linha, coluna)
+        coordenadas_bombas = []
+        while quantidade_bombas > 0:
+            coordenada = (randint(0, linha - 1), randint(0, coluna - 1))
+            if coordenada not in coordenadas_bombas:
+                coordenadas_bombas.append(coordenada)
+                quantidade_bombas-=1
+        return coordenadas_bombas
 
-    def __total_bombas(self):
-        return self.tamanho/2
+    def __total_bombas(self, linha, coluna):
+        return int((linha*coluna)/3)
 
-    def __str__(self):
-        return str(self.tabuleiro)
+    def imprimir_tabuleiro(self):
+        for posicao in self.__tabuleiro:
+            print(str(posicao))
+    
+    def jogada(self, linha, coluna):
+        """ 1. Verifica se as coordenadas são válidas
+            2. Validar se acertei uma mina: 
+                caso sim:
+                    Game Over 
+                caso não: 
+                    marcar a posição escolhida no tabuleiro com a quantidade de 
+                    bombas existentes nos nós vizinhos """
+        raise NotImplementedError("Método não implementado")
 
-o = CampoMinado(10)
-for linha in o.tabuleiro:
-    print(str(linha))
