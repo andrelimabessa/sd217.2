b1f72f735e860637f89d0d2022c6980420db9e53
rubens ribeiro de aguiar <1727555>
2017-08-31 20:43:34 -0300

    Falta Contar os vizinhos

--- 1.CampoMinadoMonolitico/campo_minado_negocio.py
+++ 1.CampoMinadoMonolitico/campo_minado_negocio.py
@@ -15,16 +15,41 @@ class CampoMinado:
     def __distribuir_bombas(self, linha, coluna):
         quantidade_bombas = self.__total_bombas(linha, coluna)
         coordenadas_bombas = []
-        while quantidade_bombas > 0:
+        while quantidade_bombas > 1:
             coordenada = (randint(0, linha - 1), randint(0, coluna - 1))
             if coordenada not in coordenadas_bombas:
                 coordenadas_bombas.append(coordenada)
                 quantidade_bombas-=1
+        """ Adicionado por mim para testar uma bomba conhecida """        
+        coordenada = (1,1)
+        coordenadas_bombas.append(coordenada)
         return coordenadas_bombas
 
     def __total_bombas(self, linha, coluna):
         return int((linha*coluna)/3)
 
+    def __coordenadas_validas(self, linha, coluna):
+        if linha not in range(0,self.__linha):
+            print("Linha Invalida")
+            return False
+        elif coluna not in range(0,self.__coluna):
+            print("Coluna Invalida")
+            return False
+        return True
+
+    def __conta_bombas_vizinho(self, linha, coluna):
+        pass
+
+    def __jogada_result(self, linha, coluna):
+        cont = 0
+        for l in range(linha-1,linha+1):
+            for c in range(coluna-1,coluna+1): 
+                posicao = (l,c)
+                if posicao in self.__coordenadas_bombas:
+                    cont += 1
+            
+        self.__tabuleiro[linha][coluna] = str(cont)
+                
     def imprimir_tabuleiro(self):
         for posicao in self.__tabuleiro:
             print(str(posicao))
@@ -37,5 +62,10 @@ class CampoMinado:
                 caso não: 
                     marcar a posição escolhida no tabuleiro com a quantidade de 
                     bombas existentes nos nós vizinhos """
-        raise NotImplementedError("Método não implementado")
-
+        if self.__coordenadas_validas(linha,coluna):
+                    
+            posicao = (linha,coluna)
+            if posicao in self.__coordenadas_bombas:
+                print("Fim de jogo!!")
+            else:
+                self.__jogada_result(linha,coluna)
