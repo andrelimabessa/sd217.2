5c6f5898598e4e78e99044d2134a84be32d100ef
Marcelo Silva <marcelosilva.developer@gmail.com>
2017-09-11 22:26:44 -0300

    Implementa método de jogada

--- 1.CampoMinadoMonolitico/campo_minado_negocio.py
+++ 1.CampoMinadoMonolitico/campo_minado_negocio.py
@@ -8,7 +8,7 @@ class CampoMinado:
         self.__coluna = coluna
         self.__tabuleiro = self.__inicializar_tabuleiro(linha, coluna)
         self.__coordenadas_bombas = self.__distribuir_bombas(linha,coluna)
-        
+
     def __inicializar_tabuleiro(self, linha, coluna):
         return [['X' for x in range(coluna)] for j in range(linha)]
 
@@ -28,14 +28,37 @@ class CampoMinado:
     def imprimir_tabuleiro(self):
         for posicao in self.__tabuleiro:
             print(str(posicao))
-    
-    def jogada(self, linha, coluna):
-        """ 1. Verifica se as coordenadas são válidas
-            2. Validar se acertei uma mina: 
-                caso sim:
-                    Game Over 
-                caso não: 
-                    marcar a posição escolhida no tabuleiro com a quantidade de 
-                    bombas existentes nos nós vizinhos """
-        raise NotImplementedError("Método não implementado")
 
+    def __posicao_valida(self, linha, coluna):
+        linhas = range(0, self.__linha)
+        colunas = range(0, self.__coluna)
+
+        return linha in linhas and coluna in colunas
+
+    def __game_over(self, linha, coluna):
+        posicao = (linha, coluna)
+
+        return posicao in self.__coordenadas_bombas
+
+    def __total_bombas_vizinhas(self, linha, coluna):
+        total = 0
+
+        for linha in range(linha - 1, linha + 1):
+            for coluna in range(coluna - 1, coluna + 1):
+                if (linha, coluna) in self.__coordenadas_bombas:
+                    total += 1
+
+        return total
+
+    def __jogada(self, linha, coluna):
+        total_bombas = self.__total_bombas_vizinhas(linha, coluna)
+
+        self.__tabuleiro[linha][coluna] = str(total_bombas)
+        self.imprimir_tabuleiro()
+
+    def jogada(self, linha, coluna):
+        if self.__posicao_valida(linha, coluna):
+            if self.__game_over(linha, coluna):
+                print('===== GAME OVER =====')
+            else:
+                self.__jogada(linha, coluna)
