fc037d74e9c534ebc58134262136054e29d9eab3
carloseduardosx <carloseduardosx2015@gmail.com>
2017-09-14 15:18:45 -0300

    Implement main game function
    - Update the board index with adjacent bombs quantity
    - Improve board printing
    - Rename some variable and functions to match the pattern
    - Exit game if the player hit a bomb

--- 1.CampoMinadoMonolitico/campo_minado_negocio.py
+++ 1.CampoMinadoMonolitico/campo_minado_negocio.py
@@ -1,19 +1,23 @@
+# -*- coding: utf-8 -*-
 from random import randint
+from sys import exit
 
 class CampoMinado:
 
     def __init__(self, linha, coluna):
         """ Inicializando campo minado com linha X coluna posicoes """
-        self.__linha = linha
-        self.__coluna = coluna
+        self.__linha__ = linha
+        self.__coluna__ = coluna
         self.__tabuleiro = self.__inicializar_tabuleiro(linha, coluna)
         self.__coordenadas_bombas = self.__distribuir_bombas(linha,coluna)
-        
+
+
     def __inicializar_tabuleiro(self, linha, coluna):
         return [['X' for x in range(coluna)] for j in range(linha)]
 
+
     def __distribuir_bombas(self, linha, coluna):
-        quantidade_bombas = self.__total_bombas(linha, coluna)
+        quantidade_bombas = self.__total_bombas__(linha, coluna)
         coordenadas_bombas = []
         while quantidade_bombas > 0:
             coordenada = (randint(0, linha - 1), randint(0, coluna - 1))
@@ -22,20 +26,67 @@ class CampoMinado:
                 quantidade_bombas-=1
         return coordenadas_bombas
 
-    def __total_bombas(self, linha, coluna):
+
+    def __total_bombas__(self, linha, coluna):
         return int((linha*coluna)/3)
 
+
     def imprimir_tabuleiro(self):
+        print "-" * 50
         for posicao in self.__tabuleiro:
             print(str(posicao))
-    
-    def jogada(self, linha, coluna):
-        """ 1. Verifica se as coordenadas são válidas
-            2. Validar se acertei uma mina: 
-                caso sim:
-                    Game Over 
-                caso não: 
-                    marcar a posição escolhida no tabuleiro com a quantidade de 
-                    bombas existentes nos nós vizinhos """
-        raise NotImplementedError("Método não implementado")
+        print "-" * 50
 
+
+    def __get_adjacent_bombs_quantity__(self, line, column):
+        adjacent_bombs_quantity = 0
+        adjacent_coordinates = [
+            (line, column - 1),
+            (line, column + 1),
+            (line - 1, column - 1),
+            (line - 1, column),
+            (line - 1, column + 1),
+            (line + 1, column - 1),
+            (line + 1, column),
+            (line + 1, column + 1)
+        ]
+
+        # Eliminating adjacent coordinates from special cases
+        if line == 0:
+            indexes_to_remove = [2, 3, 4]
+            adjacent_coordinates = [v for i, v in enumerate(adjacent_coordinates) if i not in indexes_to_remove]
+        elif line == self.__linha__ - 1:
+            indexes_to_remove = [5, 6, 7]
+            adjacent_coordinates = [v for i, v in enumerate(adjacent_coordinates) if i not in indexes_to_remove]
+
+        # Eliminating adjacent coordinates from special cases
+        if column == 0:
+            indexes_to_remove = [0, 2, 5]
+            adjacent_coordinates = [v for i, v in enumerate(adjacent_coordinates) if i not in indexes_to_remove]
+        elif column == self.__coluna__ - 1:
+            indexes_to_remove = [1, 4, 7]
+            adjacent_coordinates = [v for i, v in enumerate(adjacent_coordinates) if i not in indexes_to_remove]
+
+        for coordinate in adjacent_coordinates:
+
+            if coordinate in self.__coordenadas_bombas:
+                adjacent_bombs_quantity += 1
+
+        return adjacent_bombs_quantity
+
+
+    def jogada(self, linha, coluna):
+        coordinate_to_play = (linha, coluna)
+
+        if linha < 0 or coluna < 0:
+            print "This coordinate(%r, %r) isn't valid." % coordinate_to_play
+            return
+        elif linha >= self.__linha__ or coluna >= self.__coluna__:
+            print "This coordinate(%r, %r) doesn't exist in the board." % coordinate_to_play
+            return
+
+        if coordinate_to_play in self.__coordenadas_bombas:
+            print "You hit a bomb!"
+            exit(0)
+        else:
+            self.__tabuleiro[linha][coluna] = self.__get_adjacent_bombs_quantity__(linha, coluna)
