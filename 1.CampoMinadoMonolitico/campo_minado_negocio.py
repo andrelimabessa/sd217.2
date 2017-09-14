4c21a85e1f81fbd56009298e2e2caf215042d5b5
jeferson araujo de sousa <1412160@7setlabs.local>
2017-09-14 20:29:11 -0300

    Jogadas Interativas

--- 1.CampoMinadoMonolitico/campo_minado_negocio.py
+++ 1.CampoMinadoMonolitico/campo_minado_negocio.py
@@ -1,4 +1,5 @@
 from random import randint
+from ast import literal_eval
 import os
 class CampoMinado:
 
@@ -6,9 +7,10 @@ class CampoMinado:
         """ Inicializando campo minado com linha X coluna posicoes """
         self.__linha = linha
         self.__coluna = coluna
+        self._qtd_jogadas = (linha * coluna) - self.__total_bombas(linha, coluna)
         self.__tabuleiro = self.__inicializar_tabuleiro(linha, coluna)
         self.__coordenadas_bombas = self.__distribuir_bombas(linha,coluna)
-        
+
     def __inicializar_tabuleiro(self, linha, coluna):
         return [['*' for x in range(coluna)] for j in range(linha)]
 
@@ -36,13 +38,13 @@ class CampoMinado:
         if coordenada  in self.__coordenadas_bombas:
             return True
         return False
+    def __menu(self):
+        print(". . . .  . . . . . . .Campo Minado . . . . . . .  . . . .")
+        print(". . . . @ . . . . . . . . . . . . . . @ . . . .")
 
-    def __gameOver():
-        print(". . @ . . . . . . . . . . . . . . . . . . @ . .")
-        print(". . . . @ . . . . . . . . . . . . . . @ . . . .")
-        print(". . . . . . @ BOOM!!! ÉRROOOUU ! Fastop ! @ . . . . . .")
-        print(". . . . @ . . . . . . . . . . . . . . @ . . . .")
-        print(". . @ . . . . . . . . . . . . . . . . . . @ . .")
+
+
+
 
     def __pega_vizinhos(self,linha,coluna):
         coordenada = (linha,coluna)
@@ -59,42 +61,27 @@ class CampoMinado:
 
         if coordenada1 in self.__coordenadas_bombas :
             count = count + 1
-            self.__tabuleiro[linhaLESQ][coluna] = 1
+
         elif coordenada2 in self.__coordenadas_bombas :
             count =  + 1
-            self.__tabuleiro[linhaLDIR][coluna] = 1
+
         elif coordenada3 in self.__coordenadas_bombas :
             count = count + 1
-            self.__tabuleiro[linha][linhaCAC] = 1
+
         elif coordenada4 in self.__coordenadas_bombas :
             count = count + 1
-            self.__tabuleiro[linha][linhaCAB] = 1
-        
+
+
         else:
             pass
 
-        print ("Total bombas Vizinhas ",count )
-        
+        return count
 
 
-        print (coordenada1)
-        print (coordenada2)
-        print (coordenada3)
-        print (coordenada4)
-        print (self.__coordenadas_bombas)
-        #if coordenada in self.__coordenadas_bombas:
-            #print ( coordenada[linha(-1)][coluna])
 
-        #for 
-        # for i in range(-1, 2):
-        #     for j in range(-1, 2):
-        #         if i == 0 and j == 0:
-        #             countinue
-        #         elif -1 < (linha + i) < tabuleiro and -1 < (coluna + j) < tabuleiro:
-        #             vizinhos.append((linha + i, coluna + j))
-        # return vizinhos
 
-        
+
+
 
     def __total_bombas(self, linha, coluna):
         return int((linha*coluna)/3)
@@ -104,31 +91,32 @@ class CampoMinado:
         for posicao in self.__tabuleiro:
             print(str(posicao))
 
+    def proxima_jogada(self):
+        return self._qtd_jogadas > 0
 
-    
-    def jogada(self, linha, coluna):
-        """ 1. Verifica se as coordenadas são válidas
-            2. Validar se acertei uma mina: 
-                caso sim:
-                    Game Over 
-                caso não: 
-                    marcar a posição escolhida no tabuleiro com a quantidade de 
-                    bombas existentes nos nós vizinhos """
+
+
+    def jogada(self,linha,coluna):
+
+
         if self.__coordenadas_validas(linha,coluna):
             if self.__mina_acertada(linha,coluna):
-                os.system("cls")
-                self.__pega_vizinhos(linha,coluna)
+
+
                 self.__tabuleiro[linha][coluna] = 1
                 print(". . @ . . . . . . . . . . . . . . . . . . @ . .")
                 print(". . . . @ . . . . . . . . . . . . . . @ . . . .")
                 print(". . . . . . @ BOOM!!! ÉRROOOUU ! Fastop ! @ . . . . . .")
                 print(". . . . @ . . . . . . . . . . . . . . @ . . . .")
                 print(". . @ . . . . . . . . . . . . . . . . . . @ . .")
-                
-                
+                self._qtd_jogadas = 0
+
+
             else:
                 print("Escapou Fedendo !!")
-                self.__tabuleiro[linha][coluna] = 0
-            
-       # raise NotImplementedError("Método não implementado")
+                self.__tabuleiro[linha][coluna] = self.__pega_vizinhos(linha,coluna)
+                self._qtd_jogadas-=1
 
+
+
+       # raise NotImplementedError("Método não implementado")
--- 1.CampoMinadoMonolitico/campo_minado_view.py
+++ 1.CampoMinadoMonolitico/campo_minado_view.py
@@ -3,8 +3,23 @@ from campo_minado_negocio import CampoMinado
 objeto = CampoMinado(3, 5)
 objeto.imprimir_tabuleiro()
 
+#objeto.menu()
 
-objeto.jogada(2,2)
+
+while objeto.proxima_jogada():
+    print (objeto.proxima_jogada())
+
+    linha = int(input("Entre com posicao da linha :"))
+    coluna = int(input("Entre com posicao da coluna :")) #input("Entre com posicao da coluna :")
+
+    objeto.jogada(linha,coluna)
 
 
 objeto.imprimir_tabuleiro()
+
+""" 1. Menu para iniciar o jogo
+    2. Menu declara jogada
+    3. Regra pra vitoria
+
+    4. Salvar jogadas
+    5.Continuar jogo"""
