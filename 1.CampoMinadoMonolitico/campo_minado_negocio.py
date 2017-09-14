de97f89acff012b5c52ceb82268ee31ac23cf319
rubensra <rubens.r.a@gmail.com>
2017-09-14 17:29:35 -0300

    Campo minado local

--- 1.CampoMinadoMonolitico/campo_minado_negocio.py
+++ 1.CampoMinadoMonolitico/campo_minado_negocio.py
@@ -1,16 +1,18 @@
 from random import randint
+import os
 
 class CampoMinado:
 
     def __init__(self, linha, coluna):
         """ Inicializando campo minado com linha X coluna posicoes """
-        self.__linha = linha
-        self.__coluna = coluna
-        self.__tabuleiro = self.__inicializar_tabuleiro(linha, coluna)
-        self.__coordenadas_bombas = self.__distribuir_bombas(linha,coluna)
-        
+        self.__linha = linha + 1
+        self.__coluna = coluna + 1
+        self.__tabuleiro = self.__inicializar_tabuleiro(self.__linha,self.__coluna)
+        self.__coordenadas_bombas = self.__distribuir_bombas(self.__linha,self.__coluna)
+        self.__qtd_bombas = self.__total_bombas(self.__linha,self.__coluna)
+
     def __inicializar_tabuleiro(self, linha, coluna):
-        
+
         board = [['X' for x in range(coluna)] for j in range(linha)]
         for l in range(linha):
             for c in range(coluna):
@@ -21,25 +23,23 @@ class CampoMinado:
                 elif l != 0 and c == 0:
                     board[l][c] = str(l)
                 else:
-                    board[0][0] = ' '
-                    
+                    board[l][c] = ' '
+
         return board
 
     def __distribuir_bombas(self, linha, coluna):
         quantidade_bombas = self.__total_bombas(linha, coluna)
         coordenadas_bombas = []
-        while quantidade_bombas > 1:
-            coordenada = (randint(1, linha - 1), randint(1, coluna - 1))
+        while quantidade_bombas > 0:
+            coordenada = (randint(1, linha-1), randint(1, coluna-1))
             if coordenada not in coordenadas_bombas:
                 coordenadas_bombas.append(coordenada)
                 quantidade_bombas-=1
-        """ Adicionado por mim para testar uma bomba conhecida """        
-        coordenada = (1,1)
-        coordenadas_bombas.append(coordenada)
+
         return coordenadas_bombas
 
     def __total_bombas(self, linha, coluna):
-        return int((linha-1*coluna-1)/3)
+        return int(((linha-1)*(coluna-1))/3)
 
     def __coordenadas_validas(self, linha, coluna):
         if linha not in range(1,self.__linha):
@@ -51,34 +51,65 @@ class CampoMinado:
         return True
 
     def __conta_bombas_vizinho(self, linha, coluna):
-        pass
-
-    def __jogada_result(self, linha, coluna):
         cont = 0
-        for l in range(linha-1,linha+1):
-            for c in range(coluna-1,coluna+1): 
-                posicao = (l,c)
-                if posicao in self.__coordenadas_bombas:
-                    cont += 1
-            
-        self.__tabuleiro[linha][coluna] = str(cont)
-                
+        for l in range(linha-1,linha+2):
+            if(l > 0) and (l<= self.__linha):
+                for c in range(coluna-1,coluna+2):
+                    if(c > 0) and (c <= self.__coluna):
+                        posicao = (l,c)
+                        if posicao in self.__coordenadas_bombas:
+                            cont += 1
+                            
+        return cont
+
+    """ Funcao para imprimir o tabuleiro na Tela. """
     def imprimir_tabuleiro(self):
         for posicao in self.__tabuleiro:
             print(str(posicao))
-    
-    def jogada(self, linha, coluna):
+
+    def __jogada(self, linha, coluna):
         """ 1. Verifica se as coordenadas são válidas
-            2. Validar se acertei uma mina: 
+            2. Validar se acertei uma mina:
                 caso sim:
-                    Game Over 
-                caso não: 
-                    marcar a posição escolhida no tabuleiro com a quantidade de 
+                    Game Over
+                caso não:
+                    marcar a posição escolhida no tabuleiro com a quantidade de
                     bombas existentes nos nós vizinhos """
-        if self.__coordenadas_validas(linha,coluna):
-                    
+        
+        if self.__coordenadas_validas(linha,coluna):         
             posicao = (linha,coluna)
-            if posicao in self.__coordenadas_bombas:
-                print("Fim de jogo!!")
+            if posicao not in self.__coordenadas_bombas:
+                self.__tabuleiro[linha][coluna] = str(self.__conta_bombas_vizinho(linha,coluna))
+                return False
             else:
-                self.__jogada_result(linha,coluna)
+                print("KABOOOM!!! Fim de jogo!!")
+                return True
+
+    """ Funcao Principal do Jogo """   
+    def principal(self):
+        """ Total de Jogadas """
+        jogadas = 0 
+
+        """ Variavel de Controle do Loop """
+        perdeu = False
+
+        """ Loop principal do Jogo """     
+        while not(perdeu):
+            unused_variable = os.system("clear")
+            self.imprimir_tabuleiro()
+            lin = int(input("Digite a linha: "))
+            col = int(input("Digite a coluna: "))
+            perdeu = self.__jogada(lin,col)
+            jogadas = jogadas + 1
+            if (((self.__linha-1)*(self.__coluna-1))-jogadas) == int(self.__qtd_bombas):
+                print("\nPARABENS!! VOCE VENCEU!!!")
+                perdeu = True
+
+        """ Teste para continuar ou sair. """
+        flag = str(input("Jogar de novo S/N?"))
+        if(flag == 's' or flag == 'S'):
+            """ Se Quiser continuar Verdadeiro """
+            return True
+        else:
+            """ Caso contrario Falso """
+            return False
--- 1.CampoMinadoMonolitico/campo_minado_view.py
+++ 1.CampoMinadoMonolitico/campo_minado_view.py
@@ -1,6 +1,17 @@
 from campo_minado_negocio import CampoMinado
+import os
+"""objeto = CampoMinado(10, 10)"""
+"""objeto.imprimir_tabuleiro()"""
+"""objeto.jogada(9,9)"""
+"""objeto.imprimir_tabuleiro()"""
+jogar = True
+while(jogar):
+    unused_variable = os.system("clear")
+    linMax = int(input("Digite o total de linhas: "))
+    colMax = int(input("Digite o total de colunas: "))
+    objeto = CampoMinado(linMax,colMax)
+    jogar = objeto.principal()
+    
 
-objeto = CampoMinado(10, 10)
-objeto.imprimir_tabuleiro()
-objeto.jogada(1,2)
-objeto.imprimir_tabuleiro()
+
+
