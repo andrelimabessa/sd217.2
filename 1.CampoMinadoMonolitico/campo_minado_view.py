613a6e1b884d77671fcb722f811c9dc6f5c0a2ff
André Bessa <andrebessa@gmail.com>
2017-09-18 14:41:45 -0300

    descrição do trabalho de SD

--- 1.CampoMinadoMonolitico/campo_minado_view.py
+++ 1.CampoMinadoMonolitico/campo_minado_view.py
@@ -1,5 +1,12 @@
 from campo_minado_negocio import CampoMinado
 
-objeto = CampoMinado(10, 10)
-objeto.imprimir_tabuleiro()
-objeto.jogada(2,2)
+
+
+""" 
+    1. Menu para iniciar o jogo
+    2. Menu declara jogada
+    3. Regra pra vitória
+    
+    4. Salvar jogadas
+    5. Continuar jogo 
+ """
