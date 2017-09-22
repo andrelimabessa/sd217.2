9e05f690ce4f5e7db08ca82498cbdc8c8d1a5a23
Lucas Almeida <lucas.almeida1508@gmail.com>
2017-09-21 21:45:17 -0300

    vitória

--- 1.CampoMinadoMonolitico/campo_minado_view.py
+++ 1.CampoMinadoMonolitico/campo_minado_view.py
@@ -1,11 +1,29 @@
 from campo_minado_negocio import CampoMinado
 
 objeto = CampoMinado(4, 4)
+print("Bom Jogo!")
 objeto.imprimir_tabuleiro()
-objeto.jogada(0,0)
-objeto.jogada(2,2)
-objeto.jogada(0,3)
-objeto.jogada(3,3)
+num_jogadas = 0
+game_won = False
 
+print(objeto.total_jogadas)
+
+tamanho = objeto.tamanho_tabuleiro()
+
+num_jogadas_max  = tamanho
+while objeto.total_jogadas > 0:
+    line = int(input("Digite uma linha: "))
+    col = int(input("Digite uma coluna: "))
+    print("Linha: " + str(line) + "\nColuna: " + str(col))
+    objeto.jogada(line, col)
+    if num_jogadas_max == 0:
+        game_won = True
+        game_over = True
+        print("Game Won!")
+        break
+    if objeto.game_over == True and game_won == False:
+        print("========================")
+        print("Game Over!")
+        print("*****Novo Jogo*****")
 
 
