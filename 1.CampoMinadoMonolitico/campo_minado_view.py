356d99ce158c433dc2ed9eab9a99c05377c8b308
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-10-08 16:01:47 -0300

    Corrigo bug inicio

--- 1.CampoMinadoMonolitico/campo_minado_view.py
+++ 1.CampoMinadoMonolitico/campo_minado_view.py
@@ -3,26 +3,10 @@ from os.path import isfile
 from os import remove
 import json
 import sys
-from udp_server import server
-from udp_cliente import client
 
 
 
-print("Você quer executar:")
-print("1 para servidor")
-print("2 para cliente")
-opcao = input("Opção:")
 
-try:
-    if int(opcao) == 1:
-        print("Servidor ativado:\n")
-        server()
-    elif int(opcao) == 2:
-        print("Cliente ativado:\n")
-        client()
-except : # pega todas possíveis
-    for val in sys.exc_info():
-        print(val)
 
 
 objeto = CampoMinado(2, 2)
