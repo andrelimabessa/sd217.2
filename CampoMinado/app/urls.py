5fb2062f45b7cdab28e7293eb8eeed9d1b453db2
jeferson araujo de sousa <1412160@7SETLABS.LOCAL>
2017-11-09 20:26:00 -0300

    Terminando de Configurar

--- CampoMinado/app/urls.py
+++ CampoMinado/app/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from . import views
-
-    urlpatterns = [
-		url(r'teste/$', views.post_list), 
-            
-    ]
+urlpatterns = [
+	url(r'teste/$', views.post_list),
+		
+]
--- CampoMinado/app/views.py
+++ CampoMinado/app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
-def post_list(request):
-	return render(request, 'post_list.html', {}) 
+def post_list(request):          
+	if request.method == "POST":
+		form = JogadaForm(request.POST)
+		if form.is_valid():
+			jogada = form.save(commit=False)
+			jogada.autor = request.user
+			jogada.created_date = timezone.now()
+			jogada.save()
+	else:
+		form = JogadaForm()
+
+	return render(request, 'post_list.html', {'form': form})
 # Create your views here.
