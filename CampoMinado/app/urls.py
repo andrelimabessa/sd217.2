aae21358adb275423a6c41c65a3b011b5d573653
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-17 11:57:44 -0300

    TENTANDO

--- CampoMinado/app/urls.py
+++ CampoMinado/app/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from . import views
 urlpatterns = [
 	url(r'teste/$', views.post_list),
-	url(r'^$', views.campo),		
+	url(r'^$', views.NovoJogo),		
 ]
--- CampoMinado/app/views.py
+++ CampoMinado/app/views.py
@@ -3,20 +3,56 @@ from django.utils import timezone
 from .forms import JogadaForm
 from .models import Jogada
 from .campo_minado_negocio import CampoMinado
+
 def post_list(request):
-	if request.method == "POST":
-		form = JogadaForm(request.POST)
-		if form.is_valid():
-			jogada = form.save(commit=False)
-			jogada.autor = request.user
-			jogada.created_date = timezone.now()
-			jogada.save()
+	return render(request, 'post_list.html', {})
+
+def NovoJogo(request):
+	jogo = JogadaForm(request.POST)
+	return render(request, 'index.html', {'entrada': jogo})
+
+def Partida(request):
+	if request.method == 'POST':
+		entrada = JogadaForm(request.POST)
+		if entrada.is_valid():
+			linha = entrada.cleaned_data['linha']
+			coluna = entrada.cleaned_data['coluna']
+			objeto = CampoMinado(linha,coluna)
+			tabuleiro = objeto.retorna_tabuleiro()
+			objeto.salvarJogo()
 	else:
-		form = JogadaForm()
-	jogadas = Jogada.objects.all()
-	return render(request, 'post_list.html', {'form': form, 'jogadas':jogadas})
-# Create your views here.
+		entrada = JogadaForm()
 
-def campo(request):
+	return render(request, 'tabuleiro.html', {'entrada': entrada, 'tabuleiro': tabuleiro})
 
-	return render(request, 'index.html')
+def Principal(request):
+	objeto = CampoMinado(0,0)
+	stats = {}
+	stats = objeto.statisticas()
+	linMax = stats['linMax']
+	colMax = stats['colMax']
+	totBomb = stats['totBombas']
+	jogadas = stats['totJogadas']
+	if request.method == 'POST':
+		entrada = JogadaForm(request.POST)
+		if entrada.is_valid():
+			linha = entrada.cleaned_data['linha']
+			coluna = entrada.cleaned_data['coluna']
+			perdeu = objeto.jogada(linha,coluna)
+			objeto.salvarJogo()
+			if perdeu == False:
+				if((((linMax-1)*(colMax-1))-(jogadas+1))==totBomb):
+					mensagem = 'PARABENS VOCE VENCEU!!!!'
+					tabuleiro = objeto.retorna_tabuleiro()
+					return render(request, 'fimJogo.html', {'tabuleiro': tabuleiro, 'mensagem': mensagem})
+			elif(perdeu == 2):
+				mensagem = 'Jogo Salvo!!'
+				return render(request, 'fimJogo.html', {'mensagem': mensagem})
+			else:
+				tabuleiro = objeto.retorna_tabuleiro()
+				boardbomba = objeto.matriz_bomba(tabuleiro)
+				mensagem = 'KABOOOM!!! Fim de jogo!!'
+				return render(request, 'fimJogo.html', {'tabuleiro': boardbomba, 'mensagem': mensagem})
+
+	tabuleiro = objeto.retorna_tabuleiro()
+	return render(request, 'tabuleiro.html', {'entrada': entrada, 'tabuleiro': tabuleiro})
